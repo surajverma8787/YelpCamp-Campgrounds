1cc3046dd89aabcd8e3ecfbd482dbc39e0ef2620
unknown <surajverma8787@gmail.com>
2022-07-11 14:14:14 +0530

    Deleted Reviews with Camp using Middleware

--- models/campground.js
+++ models/campground.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Review = require("./review");
 const CampgroundSchema = new Schema({
     title: String,
     image: String,
@@ -14,5 +15,16 @@ const CampgroundSchema = new Schema({
     ]
 });
 
+CampgroundSchema.post('findOneAndDelete', async (doc) => {
+    if (doc) {
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+
+    }
+
+})
 //This is basically a constructor for model Campgrounds;
 module.exports = mongoose.model('Campgrounds', CampgroundSchema);
