166d6385dd19196b63ab8684be55abe431272433
unknown <surajverma8787@gmail.com>
2022-07-24 19:53:14 +0530

    Logout Added

--- routes/user.js
+++ routes/user.js
@@ -35,4 +35,13 @@ router.post("/login", passport.authenticate('local', { failureFlash: true, failu
     res.redirect("/campgrounds");
 });
 
+router.get("/logout", (req, res) => {
+    req.logout(function (err) {
+        if (err) { return next(err); }
+        req.flash('success', "Logged Out");
+        res.redirect("/campgrounds");
+    });
+
+})
+
 module.exports = router;
