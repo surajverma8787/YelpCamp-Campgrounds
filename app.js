478fdbcc40de991c764bf47185895db1ee40d45c
unknown <surajverma8787@gmail.com>
2022-07-24 20:08:27 +0530

    Hiding Login Logout Register in NavBar

--- app.js
+++ app.js
@@ -73,6 +73,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
