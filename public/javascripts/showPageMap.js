0b6c5a355c4d1b78a74733644fbbe3bd014541bd
unknown <surajverma8787@gmail.com>
2022-07-31 01:34:31 +0530

    Customizing Map Popup

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -9,4 +9,10 @@ const map = new mapboxgl.Map({
 
 new mapboxgl.Marker()
     .setLngLat(campgrounds.geometry.coordinates)
+    .setPopup(
+        new mapboxgl.Popup({ offset: 25 })
+            .setHTML(
+                '<h3>' + campgrounds.title + '</h3>' + '<p>' + campgrounds.location + '</p>'
+            )
+    )
     .addTo(map)
